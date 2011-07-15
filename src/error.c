21af10adb5ea9e9f53986d9d8cd12b6f5cb534ec
Kim Burgestrand <kim@burgestrand.se>
2011-07-15 19:23:03 +0200

    Add Error subsystem

---
+++ src/error.c
@@ -0,0 +1,10 @@
+#include "libmockspotify.h"
+
+/*** MockSpotify API ***/
+const char*
+sp_error_message(sp_error error)
+{
+    char *buff = malloc(20 * sizeof(char));
+    sprintf(buff, "sp_error: %d", error);
+    return buff;
+}
