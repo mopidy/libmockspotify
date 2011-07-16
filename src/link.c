26563f6f3fa20e74ca196c87cdd016bf953ecdc0
Kim Burgestrand <kim@burgestrand.se>
2011-07-16 02:31:51 +0200

    Add sp_link_type

--- src/link.c
+++ src/link.c
@@ -30,3 +30,22 @@ sp_link_as_string(sp_link *link, char *buffer, int buffer_size)
 
   return strlen(link->data);
 }
+
+sp_linktype
+sp_link_type(sp_link *link)
+{
+  #define LINK_IS(type) (strstr(link->data, ":" #type))
+  #define LINK_CASE_FOR(string, type) if (LINK_IS(string)) { return SP_LINKTYPE_##type; }
+
+  LINK_CASE_FOR(search, SEARCH);
+  LINK_CASE_FOR(track,  TRACK);
+  LINK_CASE_FOR(album,  ALBUM);
+  LINK_CASE_FOR(artist, ARTIST);
+
+  // Order of these three is significant
+  LINK_CASE_FOR(playlist, PLAYLIST);
+  LINK_CASE_FOR(starred,  STARRED);
+  LINK_CASE_FOR(user,     PROFILE);
+
+  return SP_LINKTYPE_INVALID;
+}
