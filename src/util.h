c1edd1358355fd02d8687f679cb4486ad43a6cce
Kim Burgestrand <kim@burgestrand.se>
2011-07-16 02:43:26 +0200

    Fix STARTS_WITH macro comparing 1 less character than needed

--- src/util.h
+++ src/util.h
@@ -6,6 +6,6 @@
 #define ALLOC(type) ((type*) xmalloc(sizeof(type)))
 void *xmalloc(size_t);
 
-#define STARTS_WITH(x, y) (strncmp((x), (y), sizeof(y) - 1) == 0)
+#define STARTS_WITH(x, y) (strncmp((x), (y), strlen(y)) == 0)
 
 #endif /* UTIL_H */
