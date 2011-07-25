7a02c1174d819be374c24fd8b406ff3d66c9bbab
Kim Burgestrand <kim@burgestrand.se>
2011-07-25 05:01:10 +0200

    Add MEMCPY and MEMCPY_N macros

--- src/util.h
+++ src/util.h
@@ -5,6 +5,8 @@
 
 #define ALLOC(type) ALLOC_N(type, 1)
 #define ALLOC_N(type, n) ((type*) xmalloc(sizeof(type) * (n)))
+#define MEMCPY(dst, src, type) MEMCPY_N(dst, src, type, 1)
+#define MEMCPY_N(dst, src, type, n) (memcpy((dst), (src), sizeof(type) * (n)))
 void *xmalloc(size_t);
 char *hextoa(const char *, int);
 
