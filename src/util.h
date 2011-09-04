af94158c6819f384f0f5b88dd00b0dc696a04d73
Kim Burgestrand <kim@burgestrand.se>
2011-09-04 02:29:50 +0200

    Add DEFINE_X macros, for ref counters, readers and session readers

--- src/util.h
+++ src/util.h
@@ -17,4 +17,20 @@ char *strclone(const char *string);
 
 #define STARTS_WITH(x, y) (strncmp((x), (y), strlen(y)) == 0)
 
+#define DEFINE_REFCOUNTERS_FOR(type)       \
+  void sp_##type##_add_ref(sp_##type *x) {} \
+  void sp_##type##_release(sp_##type *x) {}
+
+#define DEFINE_READER(return_type, kind, field) \
+  return_type sp_##kind##_##field(sp_##kind *x) \
+  {                                             \
+    return x->field;                            \
+  }
+
+#define DEFINE_SESSION_READER(return_type, kind, field) \
+  return_type sp_##kind##_##field(sp_session *x, sp_##kind *y) \
+  {                                                     \
+    return y->field;                                    \
+  }
+
 #endif /* UTIL_H */
