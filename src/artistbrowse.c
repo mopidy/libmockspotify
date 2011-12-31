75da5bf0605a62269b35f31095fb71a3242575f3
Antoine Pierlot-Garcin <antoine@bokbox.com>
2011-12-31 13:51:38 +0100

    Call the callback after creating an artist browser.

--- src/artistbrowse.c
+++ src/artistbrowse.c
@@ -67,10 +67,11 @@ sp_artistbrowse_is_loaded(sp_artistbrowse *artistbrowse)
 }
 
 sp_artistbrowse *
-sp_artistbrowse_create(sp_session *UNUSED(session), sp_artist *artist, sp_artistbrowse_type UNUSED(type), artistbrowse_complete_cb *UNUSED(callback), void *UNUSED(userdata))
+sp_artistbrowse_create(sp_session *UNUSED(session), sp_artist *artist, sp_artistbrowse_type UNUSED(type), artistbrowse_complete_cb *callback, void *userdata)
 {
   sp_link *link = sp_link_create_from_artist(artist);
   char *artistbrowse_link;
+  sp_artistbrowse *browser;
 
   if (link == NULL || sp_link_type(link) != SP_LINKTYPE_ARTIST)
   {
@@ -79,5 +80,8 @@ sp_artistbrowse_create(sp_session *UNUSED(session), sp_artist *artist, sp_artist
 
   artistbrowse_link = ALLOC_STR(strlen("spotify:artistbrowse:1xvnWMz2PNFf7mXOSRuLws"));
   sprintf(artistbrowse_link, "spotify:artistbrowse:%s", link->data + strlen("spotify:artist:"));
-  return (sp_artistbrowse *)registry_find(artistbrowse_link);
+  browser = (sp_artistbrowse *)registry_find(artistbrowse_link);
+  if (callback)
+      callback(browser, userdata);
+  return browser;
 }
