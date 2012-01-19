8c51f891340d4d82f1a0cfe50a967a5dbb645496
Antoine Pierlot-Garcin <antoine@bokbox.com>
2012-01-19 12:35:05 +0100

    Default image value in the image_changed playlist callback.

--- src/event.c
+++ src/event.c
@@ -81,7 +81,7 @@ mocksp_event_playlist(event_type event, sp_playlist *p)
 
     case MOCK_PLAYLIST_IMAGE_CHANGED:
         if (p->callbacks->image_changed)
-            p->callbacks->image_changed(p, NULL,
+            p->callbacks->image_changed(p, "01234567890123456789",
                                         p->userdata);
         break;
 
